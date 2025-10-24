136f71634ef4f214d2fc7e7578f5a7c919c4383f
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2025-10-24 21:32:10 +0600

    added firebase secret file creat

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBee90xpuVCF0FVRgrRK-2sgS-EYUawanU",
-  authDomain: "assignment09-game-hub.firebaseapp.com",
-  projectId: "assignment09-game-hub",
-  storageBucket: "assignment09-game-hub.firebasestorage.app",
-  messagingSenderId: "607248944727",
-  appId: "1:607248944727:web:6d1414e7c2c80c2c6a1d12"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
