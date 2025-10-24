9063a45713c0ffd5407855a5b50d66d882f50cf8
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2025-10-24 19:08:50 +0600

    added forgot section back function and title

--- src/components/ForgetPasswoerd/ForgetPassword.jsx
+++ src/components/ForgetPasswoerd/ForgetPassword.jsx
@@ -1,22 +1,24 @@
 import React, { use, useState } from 'react';
 import { AuthContext } from '../../Context/AuthProvider';
-import { useLocation } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
+import { TiArrowBack } from 'react-icons/ti';
 
 const ForgetPassword = () => {
+    const navigate = useNavigate()
     const {forgetPassword} = use(AuthContext)
     const location = useLocation()
-    console.log(location)
+    // console.log(location)
 
     const passedEmail = location.state?.email || "";
      const [email, setEmail] = useState(passedEmail);
-    console.log(passedEmail)
+    // console.log(passedEmail)
 
     const handleReset = (e) => {
     e.preventDefault();
     forgetPassword(email)
       .then(() => {
         alert("Password reset link sent! Check your Gmail.");
-        window.location.href = "https://mail.google.com"; // Gmail redirect
+        window.open("https://mail.google.com", "_blank");  
       })
       .catch((error) => {
         console.log(error.code, error.message);
@@ -24,6 +26,7 @@ const ForgetPassword = () => {
   };
     return (
          <div className="hero bg-base-200 min-h-[82vh]">
+            <title>GameHub-forgot-password</title>
       <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
         <div className="card-body">
           <h1 className="text-3xl font-bold text-center">Reset Password</h1>
@@ -41,6 +44,11 @@ const ForgetPassword = () => {
             </button>
           </form>
         </div>
+         <button onClick={() => navigate(-1)} className="btn text-lg flex items-center justify-center px-8 btn-primary absolute mt-60 ml-2"> 
+            <span><TiArrowBack className="text-xl" />
+            </span>
+            <span> Back</span>
+        </button>
       </div>
     </div>
     );
